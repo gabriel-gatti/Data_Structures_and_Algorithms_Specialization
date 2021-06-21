7de0955bac315b8ec49c689e2d3b7310d82f9a9a
Gabriel <gabrielgatti@ymail.com>
2021-06-21 20:47:43 -0300

    Week 3 Algorithms on Graphs Complete

--- Algorithms_on_Graphs/week3_paths_in_graphs1/bfs.py
+++ Algorithms_on_Graphs/week3_paths_in_graphs1/bfs.py
@@ -1,15 +1,43 @@
 #Uses python3
 
 import sys
-import queue
 
 def distance(adj, s, t):
     #write your code here
+    dist = [float('inf')]*len(adj)
+    dist[s], q = 0, [s]
+
+    while q:
+        node = q.pop(0)
+        for i in adj[node]:    
+            if dist[i] == float('inf'):
+                q.append(i)
+                dist[i] = dist[node] + 1
+                if i == t:
+                    return dist[t]
+
     return -1
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = list(map(int, input.split()))
+
+    #data = [4,4,
+    #        1,2,
+    #        4,1,
+    #        2,3,
+    #        3,1,
+    #        2,4]
+    # 2
+
+    #data = [5,4,
+    #        5,2,
+    #        1,3,
+    #        3,4,
+    #        1,4,
+    #        3,5]
+    # -1
+    
     n, m = data[0:2]
     data = data[2:]
     edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
--- Algorithms_on_Graphs/week3_paths_in_graphs1/bipartite.py
+++ Algorithms_on_Graphs/week3_paths_in_graphs1/bipartite.py
@@ -1,15 +1,46 @@
 #Uses python3
 
 import sys
-import queue
+#import queue
 
 def bipartite(adj):
     #write your code here
-    return -1
+    dist = [float('inf')]*len(adj)
+    q = []
+    
+    for i, v in enumerate(adj):
+        if len(v) and dist[i] == float('inf'):
+            dist[i], s, q = 1, i, [i]
+            
+            while q:
+                node = q.pop(0)
+
+                for j in adj[node]:
+                    if dist[j] == float('inf'):
+                        dist[j] = -dist[node]
+                        q.append(j)
+
+                    elif dist[j] == dist[node]:
+                        return 0
+
+    return 1
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = list(map(int, input.split()))
+    #data = [4,4,
+    #        1,2,
+    #        4,1,
+    #        2,3,
+    #        3,1]
+    # 0
+
+    #data = [5,4,
+    #        5,2,
+    #        4,2,
+    #        3,4,
+    #        1,4]
+    # 1
     n, m = data[0:2]
     data = data[2:]
     edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
